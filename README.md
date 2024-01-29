a79434a873cce2961974d5884ab09d43982fc96d
Sumukh Bhandarkar <sumukhbhandarkar@gmail.com>
2024-01-30 02:41:35 +0530

    Updated README, Added code for BruteForce method

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Problem Statement
 <hr>
 Find all patterns in an array of intgers
+
 * The length of pattern is >= 2 integers.
 * Patterns must occur in sequential integers within the array.
 * Output will contain the pattern and the number of times it occurs in the list.
@@ -14,7 +15,6 @@ _output_ - <br>
 [1,2,3], 2 <br>
 [2,3], 2 <br>
 [3,5], 2 <br>
-[1,2,3,5,6,0,1,2,3,5,2,5], 1
 
 #### Explanation
 * Each of the pattern mentioned above is occurring multiple times in the array
--- src/main/java/org/example/BruteForce.java
+++ src/main/java/org/example/BruteForce.java
@@ -1,7 +1,58 @@
 package org.example;
 
-public class Main {
+// 1,2,3,5,6,0,1,2,3,5,2,5
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * This is the bruteforce approach, will take into account each pattern in a pair, triplet and so on
+ * It will match the pattern within the array and return the count
+ */
+public class BruteForce {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] input = new Integer[]{1,2,3,5,6,0,1,2,3,5,2,5};
+        List<Integer> inputArray = Arrays.asList(input);
+        List<List<Integer>> subArrays = findAllSubArrays(input);
+        HashMap<List<Integer>, Integer> output = new HashMap<>();
+        for (List<Integer> each : subArrays) {
+            if (countSubListFreq(inputArray, each) >= 2)
+                output.put(each, countSubListFreq(inputArray, each));
+        }
+        System.out.println(output);
+    }
+
+    public static int countSubListFreq(List<Integer> outer, List<Integer> inner) {
+        int count = 0;
+        for (int i=0; i<=outer.size() - inner.size(); i++) {
+            if (outer.subList(i, i+inner.size()).equals(inner))
+                count++;
+        }
+        return count;
+    }
+
+    /**
+     * Will return all the subArrays for the input array
+     * output: [[1, 2], [1, 2, 3], [1, 2, 3, 5], [1, 2, 3, 5, 6], [2, 3], [2, 3, 5], [2, 3, 5, 6], [2, 3, 5, 6, 0], [3, 5], [3, 5, 6], [3, 5, 6, 0], [3, 5, 6, 0, 1], [5, 6], [5, 6, 0], [5, 6, 0, 1], [5, 6, 0, 1, 2], [6, 0], [6, 0, 1], [6, 0, 1, 2], [6, 0, 1, 2, 3], [0, 1], [0, 1, 2], [0, 1, 2, 3], [0, 1, 2, 3, 5], [1, 2], [1, 2, 3], [1, 2, 3, 5], [1, 2, 3, 5, 2], [2, 3], [2, 3, 5], [2, 3, 5, 2], [2, 3, 5, 2, 5], [3, 5], [3, 5, 2], [3, 5, 2, 5], [5, 2], [5, 2, 5], [2, 5]]
+     * @param input
+     * @return
+     */
+    public static List<List<Integer>> findAllSubArrays(Integer[] input) {
+        List<List<Integer>> allSubArrays = new ArrayList<>();
+        for (int i = 0; i < input.length; i++)
+            for (int j = i; j < input.length; j++) {
+                List<Integer> subArray = new ArrayList<>();
+                for (int k = i; k <=j; k++)
+                    subArray.add(input[k]);
+                if (subArray.size() >= 2 && subArray.size() <= input.length/2-1)
+                    allSubArrays.add(subArray);
+            }
+        return allSubArrays;
     }
 }
+
+
+/**
+ * The output of the code: {[1, 2, 3]=2, [1, 2]=2, [2, 3]=2, [2, 3, 5]=2, [3, 5]=2, [1, 2, 3, 5]=2}
+ * Where each key of the HashMap shows the pattern and the value shows the count
+ */
